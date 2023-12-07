3731dfa5e56a65dd3298ccae26fc243424640969
MOHAMED <mohamednouaoui5@gmail.com>
2023-12-07 17:00:34 +0100

    mohamed modified

--- FILTER.php
+++ FILTER.php
@@ -18,7 +18,7 @@ if(isset($_GET['array']) && $_GET['array'] !=='') {
     $result = $filter->get_result();
     while($row = $result->fetch_assoc()) {
         ?>
-        <div class="card h-auto col-sm px-0 mx-0" data-key="<?php echo $articleID?>">
+        <div onclick="attachClickListeners(<?php echo $row['article_id']?>)" class="card h-auto col-sm px-0 mx-0" data-key="<?php echo $row['article_id']?>">
                         <h1><?php echo $row['article_title']?></h1>
                         <h3><?php echo $row['article_text']?></h3>
                     </div>
