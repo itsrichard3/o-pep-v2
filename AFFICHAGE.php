96f2de973507b201b666b241ebd0736a26b371cf
MesVortex <meskinemostafa4@gmail.com>
2023-12-08 16:40:38 +0100

    git

--- AFFICHAGE.php
+++ AFFICHAGE.php
@@ -8,7 +8,7 @@ if(isset($_GET['themesearch'])){
     while($row = $result->fetch_assoc()) {
         $articleID = $row['article_id'];
         ?>
-        <div class="card h-auto col-sm px-0 mx-0" data-key="<?php echo $row['article_id']?>">
+        <div onclick="attachClickListeners(<?php echo $articleID?>)" class="card h-auto col-sm px-0 mx-0" data-key="<?php echo $row['article_id']?>">
                         <h1><?php echo $row['article_title']?></h1>
                         <h3><?php echo $row['article_text']?></h3>
                     </div>
