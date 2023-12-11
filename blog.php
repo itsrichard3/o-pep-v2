19f04229842bb0ad533dd11cb97334ef8b918f2d
MesVortex <meskinemostafa4@gmail.com>
2023-12-11 12:30:48 +0100

    last changes

--- blog.php
+++ blog.php
@@ -18,20 +18,20 @@ elseif(isset($_SESSION["user_id"])){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
   </head>
   <body>
-    <div class="container">
+    <div class="container grid grid-cols-2 ml-8  ">
     <?php
     $article = $con->prepare("SELECT * FROM THEME");
     $article->execute();
     $result = $article->get_result();
     while($row = $result->fetch_assoc()) {
         ?>
-        <div class="card" data-value="<?php echo $row['theme_id']?>">
-        <h1><?php echo $row['theme_name']?></h1>
+        <div class="card mt-5  max-w-xl text-center shadow-xl shadow-black  border border-green-500 rounded-xl transition-transform duration-300 ease-in-out transform hover:scale-110   " data-value="<?php echo $row['theme_id']?>">
+        <h1 class="font-mono mb-5 underline text-2xl"><?php echo $row['theme_name']?></h1>
         </div>
-    
+
         <?php
     }
         ?>
--- tags.php
+++ tags.php
@@ -9,12 +9,14 @@ if(isset($_GET['theme'])) {
     $select->bind_param('i',$idtheme);
     $select->execute();
     $result = $select->get_result();
+    ?>
+    <div class="flex flex-col gap-8 items-center mt-5">
+    <?php
     while($row = $result->fetch_assoc()) {
         ?>
         <input type="file" id="upload_img" class="form-control" name="article_image[]" onchange="previewImage(event)">
-        <input type="text" placeholder="TITLE...." class="form-control" name="article_title[]">
-        <input type="text" placeholder="write your text ....." class="form-control" name="article_text[]">
-        <button type="button" onclick="removeArticle(this)">DELETE</button>
+        <input type="text" placeholder="TITLE...." class="w-full h-10 border border-green-700 rounded-xl pl-10" name="article_title[]">
+        <input type="text" placeholder="write your text ....." class="w-full h-10 border border-green-700 rounded-xl pl-10" name="article_text[]">
         <div class="checkboxes d-flex flex-column">
             <?php
             $tag=$con->prepare("SELECT * FROM theme_tag
@@ -27,21 +29,26 @@ if(isset($_GET['theme'])) {
             $number = $_GET['articleCounter'];
             while($row = $result->fetch_assoc()) {
                 ?>
-                <div>
+                <div class=" border border-green-500 mb-5 rounded-xl pl-3 pr-4 h-7">
                 <input type="checkbox" class="form-check-input" value="<?php echo $row['tag_id']?>" name="tags[<?php echo $number?>][]">
                 <label for="checkbox"><?php echo $row['tag_name']?></label>
                 </div>
                 <?php
             }
             
-            echo $number;
             ?>
 
             
         </div>
+        <button type="button" class=" mb-5 border border-red-500 rounded-xl w-28 h-12 text-red-600 hover:bg-red-600 hover:text-white font-mono duration-200 ease-in" onclick="removeArticle(this)">DELETE</button>
         <?php
         
     }
+    ?>
+
+
+    </div>
+    <?php
 }
 
 
